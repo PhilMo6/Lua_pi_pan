d2b81a494093dd2c7f448f9af1b0fb0bb7292b5d
PhilMo6 <philipmowrey@gmail.com>
2016-11-12 18:54:08 -0800

    Fixed DHT22, now uses wireingPi wrapper
    The rpio library wasent fast enough at reading to do DHT readings so I
    made a wrapper for c to get wiringPi to do the work and as a bonus its
    way faster then before.

--- obj/DHT22.lua
+++ obj/DHT22.lua
@@ -2,16 +2,15 @@ local Cloneable			= require("obj.Sensor")
 local DHT22			= Cloneable:clone()
 
 --[[
-	DHT22 tempature and humidity sensor. connect as in the following
+	DHT22 tempature and humidity sensor. Connect as in the following
 	https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/wiring
 	NOT APPLICABLE TO DHT11. will work on that in the future.
-	!This is a high processer use object so use with care!
 ]]
 
 DHT22.updateCmd = "Request SenDHT22"
 
 function DHT22:initialize(pin)
-	if not _G.DHT22s then _G.DHT22s = {name='DHT22s'} table.insert(objects,DHT22s) objects["DHT22s"] = DHT22s startPollSensorEvent() end
+	if not _G.DHT22s then require("source.readDHT") _G.DHT22s = {name='DHT22s'} table.insert(objects,DHT22s) objects["DHT22s"] = DHT22s startPollSensorEvent() end
 	if not DHT22s['DHT22_' ..pin] then
 		self.config = {}
 		self:setID(pin)
@@ -19,8 +18,6 @@ function DHT22:initialize(pin)
 		self.lastTRead = 0
 		self.lastHRead = 0
 		table.insert(DHT22s,self)
-		self.gpio = RPIO(pin)
-		self:read()
 	end
 end
 
@@ -37,103 +34,33 @@ function DHT22:setName(name)
 end
 
 function DHT22:read()
-	local h,t
-	for i=1,6 do
-		h,t = self:pollSensor()
-		if h then break end
-		sleep(1)
-	end
-	if h then
+	local h,t = self:pollSensor()
+	if h and h ~= 0.0 and h ~= 0 then
+		h = math.round(h,2)
+		t = math.round(t,2)
 		local up = (self.lastHRead ~= h and true or self.lastTRead ~= t and true or nil)
 		self.lastHRead = h
 		self.lastTRead = t
 		if self.masters and up then
 			self:updateMasters()
 		end
+		return h,t
 	end
-	return h,t
 end
 
---this is based off of work found here https://github.com/javieryanez/nodemcu-modules/tree/master/dht22
 function DHT22:pollSensor()
 	collectgarbage()
-	sleep(1)
+	collectgarbage('stop')
 	local pin = self:getID()
-	local checksum
-	local checksumTest
-	local hRead = 0
-	local tRead = 0
-
-	checksum = 0
-	local bitStream = {}
-	for j = 1, 40, 1 do
-		bitStream[j] = 0
+	local sen = 22
+	local hRead,tRead
+	local count = 0
+	while not hRead and count < 10 do
+		hRead,tRead = readDHT(pin)
+		count = count + 1
 	end
-	local bitlength = 0
-	local c1=0
-	local c2=0
-
-	-- Step 1:  send out start signal to DHT22
-	self.gpio:set_direction('out')
-	self.gpio:write(1)
-	sleep(0.0015)
-	self.gpio:write(0)
-	sleep(0.004)
-	self.gpio:set_direction('in')
-
-	-- Step 2:  DHT22 send response signal
-	-- bus will always let up eventually, don't bother with timeout
-	while (self.gpio:read() == 0 ) do end
-	while (self.gpio:read() == 1 and c1 < 100) do c1 = c1 + 1 end
-	-- bus will always let up eventually, don't bother with timeout
-	while (self.gpio:read() == 0 ) do end
-	while (self.gpio:read() == 1 and c2 < 100) do c2 = c2 + 1 end
-	while (self.gpio:read() == 0) do end
-
-	-- Step 3: DHT22 send data
-	for j = 1, 40, 1 do
-		while (self.gpio:read() == 1 and bitlength < 60) do
-			bitlength = bitlength + 1
-		end
-		bitStream[j] = bitlength
-		bitlength = 0
-		while (self.gpio:read() == 0) do end
-	end
-
-	local biterr = 25
-	--DHT data acquired, process.
-	for i = 1, 16, 1 do
-		if bitStream[i] > biterr then
-			hRead = hRead + 2 ^ (16 - i)
-		end
-	end
-	for i = 1, 16, 1 do
-		if bitStream[i + 16] > biterr then
-			tRead = tRead + 2 ^ (16 - i)
-		end
-	end
-	for i = 1, 8, 1 do
-		if bitStream[i + 32] > biterr then
-			checksum = checksum + 2 ^ (8 - i)
-		end
-	end
-
-	checksumTest = (bit32.band(hRead, 0xFF) + bit32.rshift(hRead, 8) + bit32.band(tRead, 0xFF) + bit32.rshift(tRead, 8))
-	checksumTest = bit32.band(checksumTest, 0xFF)
-	if tRead > 0x8000 then
-		-- convert to negative format
-		tRead = -(tRead - 0x8000)
-	end
-
-	-- conditions compatible con float point and integer
-	if (checksumTest - checksum >= 1) or (checksum - checksumTest >= 1) then
-		hRead = nil
-	end
-
-	self.gpio:set_direction('out')
-	self.gpio:write(1)
-	return (hRead and ((hRead - (hRead % 10)) / 10).."."..(hRead % 10) or nil),(tRead and ((tRead-(tRead % 10)) / 10).."."..(tRead % 10) or nil)
-
+	collectgarbage('restart')
+	return hRead,tRead
 end
 
 function DHT22:getLastRead()
@@ -142,8 +69,10 @@ end
 
 --- Stringifier for Cloneables.
 function DHT22:toString()
-	local t,h = self:getLastRead()
-	return string.format("[DHT22] %s %s %sC %s%%",self:getID(),self:getName(),t or 'nil',h or 'nil')
+	local h,t = self:getLastRead()
+	local t2
+	if t then t2 = = t * 9 / 5  + 32 end
+	return string.format("[DHT22] %s %s %sC %sF %s%%",self:getID(),self:getName(),t or 'nil',t2 or 'nil',h or 'nil')
 end
 
 return DHT22
---
+++ source/readDHT.c
@@ -0,0 +1,115 @@
+// lua wrapper for dht
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define MAX_TIMINGS	85
+
+int data[5] = { 0, 0, 0, 0, 0 };
+
+int read_dht_data(lua_State *L)
+{
+	int DHT_PIN = lua_tonumber(L, 1);
+	uint8_t laststate	= HIGH;
+	uint8_t counter		= 0;
+	uint8_t j			= 0, i;
+
+	piHiPri (99);
+	data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+	pinMode(DHT_PIN, OUTPUT);
+    digitalWrite(DHT_PIN, HIGH);
+    delayMicroseconds(10000);
+    digitalWrite(DHT_PIN, LOW);
+    delayMicroseconds(18000);
+    /* Then pull it up for 40 microseconds */
+    digitalWrite(DHT_PIN, HIGH);
+    delayMicroseconds(40);
+    /* Prepare to read the pin */
+	pinMode(DHT_PIN, INPUT);
+
+	/* detect change and read data */
+	for ( i = 0; i < MAX_TIMINGS; i++ )
+	{
+		counter = 0;
+		while ( digitalRead( DHT_PIN ) == laststate )
+		{
+			counter++;
+			delayMicroseconds( 1 );
+			if ( counter == 255 )
+			{
+				break;
+			}
+		}
+
+		laststate = digitalRead( DHT_PIN );
+		if ( counter == 255 )
+			break;
+
+		/* ignore first 3 transitions */
+		if ( (i >= 4) && (i % 2 == 0) )
+		{
+			/* shove each bit into the storage bytes */
+			data[j / 8] <<= 1;
+			if ( counter > 16 )
+				data[j / 8] |= 1;
+			j++;
+		}
+	}
+	/*
+	 * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+	 * print it out if data is good
+	 */
+	if ( (j >= 40) &&
+	     (data[4] == ( (data[0] + data[1] + data[2] + data[3]) & 0xFF) ) )
+	{
+		float h = (float)((data[0] << 8) + data[1]) / 10;
+		if ( h > 100 )
+		{
+			h = data[0];	// for DHT11
+		}
+		float c = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
+		if ( c > 125 )
+		{
+			c = data[2];	// for DHT11
+		}
+		if ( data[2] & 0x80 )
+		{
+			c = -c;
+		}
+		lua_pushnumber(L,h);
+		lua_pushnumber(L,c);
+		pinMode(DHT_PIN, OUTPUT);
+		digitalWrite(DHT_PIN, HIGH);
+		piHiPri (0);
+		return(2);
+	}else  {
+		pinMode(DHT_PIN, OUTPUT);
+		digitalWrite(DHT_PIN, HIGH);
+		piHiPri (0);
+		return(0);
+	}
+}
+
+int main( void )
+{
+	return(0);
+}
+
+int fastReadPin(lua_State *L)
+{
+	int pin = lua_tonumber(L, 1);
+	lua_pushnumber(L,digitalRead( pin ));
+	return(1);
+}
+
+int luaopen_source_readDHT (lua_State *L) {
+	wiringPiSetupGpio();
+	lua_register(L,"readDHT",read_dht_data);
+	lua_register(L,"fastReadPin",fastReadPin);
+	return 0;
+}
