81f3708857de22f62bde1effbe1f97080cf4c97f
PhilMo6 <philipmowrey@gmail.com>
2016-11-21 09:16:53 -0800

    getting ready to do servo motor object
    made the 2 files and did prep to code the objects

---
+++ obj/ServoMotor.lua
@@ -0,0 +1,93 @@
+local Cloneable			= require("obj.Cloneable")
+local Servo			= Cloneable:clone()
+--[[
+	Object used to drive 3 position Servo motors.
+]]
+
+Servo.updateCmd = "Request ServoMs"
+
+Servo.positions = {'top','mid','bot'}
+
+--- Constructor for instance-style clones.
+function Servo:initialize(pin)
+	if not _G.servoMotors then _G.servoMotors = {name='servoMotors'} table.insert(objects,servoMotors) objects["servoMotors"] = servoMotors end
+	if not servoMotors[pin1..','..pin2..','..pin3..','..pin4] then
+		self.config = {}
+		self.position = 1
+		self:setID(pin)
+		self:setName('Servo_')
+		table.insert(servoMotors,self)
+		self:test()
+	end
+end
+
+function Servo:getPosition()
+	return (self.position and Servo.positions[self.position])
+end
+
+function Servo:setID(id)
+	if self.config.id then servoMotors[self.config.id] = nil end
+	self.config.id = id
+	servoMotors[self.config.id] = self
+end
+
+function Servo:setName(name)
+	if self.config.name then servoMotors[self.config.name] = nil end
+	self.config.name = name
+	servoMotors[self.config.name] = self
+end
+
+function Servo:getHTMLcontrol()
+	return ([[<button onclick="myFunction('Som %s test')">Test</button >]]):format(self:getName())
+end
+
+function Servo:getID()
+	return self.config.id
+end
+
+function Servo:getName()
+	return self.config.name
+end
+
+function Servo:read()
+	local r = "is " .. self:getDirection()
+	return r
+end
+
+
+function Servo:off(up)
+	resetBoost(self)
+	if self.stepping then
+		Scheduler:dequeue(self.stepping)
+		self.stepping = nil
+	end
+	for i,v in ipairs(self.pins) do
+		v:write(0)
+	end
+	if not up then self:updateMasters() end
+	return true
+end
+
+function Servo:setStep(step)
+	if self.seq[step] then
+		self.config.step = step
+		return true
+	end
+	return false
+end
+
+function Servo:getStep()
+	return self.config.step
+end
+
+
+function Servo:test()
+
+end
+
+--- Stringifier for Cloneables.
+function Servo:toString()
+	return string.format("[Servo] %s %s %s",self:getID(),self:getName(),self:getPosition())
+end
+
+return Servo
---
+++ obj/commands/ServoMotor.lua
@@ -0,0 +1,76 @@
+local Command	= require("obj.Command")
+
+--- test command
+local ServoMotor	= Command:clone()
+ServoMotor.name = "ServoMotor"
+ServoMotor.keywords	= {"servomotor","Stm","Servo","servo","Ser","ser"}
+
+--- Execute the command
+function ServoMotor:execute(input,user,par)
+	local words = string.Words(input)
+	local input1, input2, input3 ,input4,input5 = words[1],words[2],words[3],words[4],words[5]
+
+	if input2 then--input2 should be the id(pin) or index of a ServoMotor
+		local motor = servoMotors[input2] or servoMotors[tonumber(input2)]
+		if motor then --That is a vaild ServoMotor
+			if input3 then--input3 should be the order to execute
+				if ServoMotor.orders[input3] then--This is a valid order
+					return ServoMotor.orders[input3](motor,input4,input5,par == 'tcp' and user or nil)
+				else
+					return "That is not a vaild order."
+				end
+			else
+				return "Please issue an order with the command."
+			end
+		else
+			return "That is not a vaild Stepper Motor."
+		end
+	else
+		return "Please select a Stepper Motor when issuing a Stepper Motor command"
+	end
+end
+
+ServoMotor.orders = {}
+ServoMotor.orders["rename"] = function(motor,name)
+	if name then
+		motor:setName(name)
+		updateServoMotorinfo()
+		return string.format("Stepper Motor %s has been renamed %s.",motor:getID(),motor:getName())
+	else
+		return "Must supply a name to rename a Stepper Motor."
+	end
+end
+ServoMotor.orders["re"] = ServoMotor.orders["rename"]
+ServoMotor.orders["read"] = function(motor)
+	local r1 = motor:getDirection()()
+	return string.format("%s is %s",motor:getName(),r1)
+end
+ServoMotor.orders["r"] = ServoMotor.orders["read"]
+
+ServoMotor.orders["middle"] = function()
+	return string.format("Use the forward or backward commands to step motor.",motor:getName())
+end
+
+ServoMotor.orders["top"] = function(motor,count)
+	motor:off(true)
+	local step = motor:stepF(tonumber(count or 1000))
+	if step then
+		return string.format("%s is now going %s.",motor:getName(),motor:getDirection())
+	end
+end
+ServoMotor.orders["f"] = ServoMotor.orders["forward"]
+
+ServoMotor.orders["bottom"] = function(motor,count)
+	motor:off(true)
+	local step = motor:stepB(tonumber(count or 1000))
+	if step then
+		return string.format("%s is now going %s.",motor:getName(),motor:getDirection())
+	end
+end
+ServoMotor.orders["b"] = ServoMotor.orders["backward"]
+
+ServoMotor.orders["test"] = function(motor,_,_,user)
+	motor:test()
+end
+
+return ServoMotor
